a744796979924379ba26633215098600d3046294
Inessa <nazhestkinainessa@gmail.com>
2026-03-26 11:39:57 +0300

    Дизайн календаря

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 function App (){
+  let days = Array(42).fill(null);
+  const weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
   return(
-    <div className = "app-wrapper"> 
-    <header className="header">Nook Notes</header>
-    <main className="main">
-      <div className="main__container">
-        <section className="main__calendar"> 
-          <p>Текст текст текст текст текст текст текст текст текст текст текст текст 
-            текст текст текст текст р
-            </p> </section>
-      </div>
-      <section className="main__task"> <p>...</p> </section>
-    </main>
+  <div className = "app-wrapper"> 
+  <header className="header">Nook Notes</header>
+  <main className="main">
+    <div className="main__container">
+      <section className="main__calendar"> 
+        {weekDays.map((day, index) => (
+          <div key={index} className="weekday">
+            {day}
+          </div>
+        ))}
+        {days.map((item, index) => (
+          <div kay={index} className="calendar">
+            {index +1}
+          </div>
+        )) }
+      </section>
     </div>
+      <section className="main__task"> <p>...</p> </section>
+  </main>
+  </div>
   )
 }
 
