eaee8a85b7ffdae19d5b8fb2414fc288ecedf630
Inessa <nazestkinainessa@gmail.com>
2026-03-31 05:14:49 +0300

    feat: Добавила логику календаря и поддержку високосного года

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-function App (){
+function App() {
   let days = Array(42).fill(null);
-  const weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
-  return(
-  <div className = "app-wrapper"> 
-  <header className="header">Nook Notes</header>
-  <main className="main">
-    <div className="main__container">
-      <section className="main__calendar"> 
-        {weekDays.map((day, index) => (
-          <div key={index} className="weekday">
-            {day}
-          </div>
-        ))}
-        {days.map((item, index) => (
-          <div kay={index} className="calendar">
-            {index +1}
-          </div>
-        )) }
-      </section>
+  const weekDays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+  let year = 2026;
+  let month = 2;
+  let day = new Date(year, month, 1);
+  let startsFrom = day.getDay() === 0 ? 6 : day.getDay() - 1;
+  let daysInMonth = new Date(year, month + 1, 0).getDate();
+  return (
+    <div className="app-wrapper">
+      <header className="header">Nook Notes</header>
+      <main className="main">
+        <div className="main__container">
+          <section className="main__calendar">
+            {weekDays.map((day, index) => (
+              <div key={index} className="weekday">
+                {day}
+              </div>
+            ))}
+            {days.map((item, index) => {
+              let dayNumber = index - startsFrom + 1;
+              return (
+                <div key={index} className="calendar">
+                  {dayNumber > 0 && dayNumber <= daysInMonth ? dayNumber : ""}
+                </div>
+              );
+            })}
+          </section>
+        </div>
+        <section className="main__task">
+          <p>...</p>
+        </section>
+      </main>
     </div>
-      <section className="main__task"> <p>...</p> </section>
-  </main>
-  </div>
-  )
+  );
 }
 
 export default App;
