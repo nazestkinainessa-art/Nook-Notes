81e86fdd16047c1e5451af46a3069db4b5bb716f
Inessa <nazestkinainessa@gmail.com>
2026-04-05 16:58:06 +0300

    feat: добавила логику навигации по месяцам/годам и изменила структуру

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
+import { useState } from "react";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+
 function App() {
   let days = Array(42).fill(null);
   const weekDays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-  let year = 2026;
-  let month = 2;
+  const [year, setYear] = useState(2026);
+  const [month, setMonth] = useState(2);
   let day = new Date(year, month, 1);
   let startsFrom = day.getDay() === 0 ? 6 : day.getDay() - 1;
   let daysInMonth = new Date(year, month + 1, 0).getDate();
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const monthShort = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
   return (
     <div className="app-wrapper">
       <header className="header">Nook Notes</header>
       <main className="main">
         <div className="main__container">
-          <section className="main__calendar">
-            {weekDays.map((day, index) => (
-              <div key={index} className="weekday">
-                {day}
+          <section className="calendar">
+            <div className="calendar__nav">
+              <div className=""> 
               </div>
-            ))}
-            {days.map((item, index) => {
-              let dayNumber = index - startsFrom + 1;
-              return (
-                <div key={index} className="calendar">
-                  {dayNumber > 0 && dayNumber <= daysInMonth ? dayNumber : ""}
-                </div>
-              );
-            })}
+              <div className="">
+                <h3></h3>
+              </div> 
+              <div className="calendar__month-selector">
+                <LeftOutlined onClick={() => {
+                  if (month === 0) {
+                    setMonth(11);
+                    setYear(year - 1);
+                  }
+                  else {
+                    setMonth(month - 1);
+                  }
+                }}
+                />
+                <h2>{monthShort[month]} {year}</h2>
+                <RightOutlined onClick={() => {
+                  if (month === 11) {
+                    setMonth(0);
+                    setYear(year + 1)
+                  }
+                  else {
+                    setMonth(month + 1);
+                  }
+                }}
+                />
+              </div>
+              <div className="calendar__grid">
+                {weekDays.map((day, index) => (
+                  <div key={index} className="calendar__weekday">
+                    {day}
+                  </div>
+                ))}
+                {days.map((item, index) => {
+                  let dayNumber = index - startsFrom + 1;
+                  return (
+                    <div key={index} className="calendar__day">
+                      {dayNumber > 0 && dayNumber <= daysInMonth
+                        ? dayNumber
+                        : ""}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </section>
         </div>
-        <section className="main__task">
-          <p>...</p>
-        </section>
+        <section className="main__task"></section>
       </main>
     </div>
   );
