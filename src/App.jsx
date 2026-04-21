2df2f7ded5a83428d26cc6be9e417c75d7842b83
Inessa <nazhestkinainessa@gmail.com>
2026-04-21 17:48:50 +0300

    feat: добавила модальное окно заметок и отображение выбранной даты

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ function App() {
   let days = Array(42).fill(null);
   const weekDays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
   const [year, setYear] = useState(new Date().getFullYear());
-  const [month, setMonth] = useState(new Date().getMonth());;
+  const [month, setMonth] = useState(new Date().getMonth());
   let day = new Date(year, month, 1);
   let startsFrom = day.getDay() === 0 ? 6 : day.getDay() - 1;
   let daysInMonth = new Date(year, month + 1, 0).getDate();
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const monthShort = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+  const monthShort = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+  const monthLittle = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   let today = new Date();
   let [selectedDay, setSelectedDay] = useState(null);
+  const [screen, setScreen] = useState('calendar');
   return (
     <div className="app-wrapper">
       <header className="header">Nook Notes</header>
@@ -71,6 +73,7 @@ function App() {
                     onClick={() => {
                       if (dayNumber > 0 && dayNumber <= daysInMonth) {
                         setSelectedDay(dayNumber);
+                        setScreen('task');
                       }
                     }}
                     >
@@ -81,9 +84,31 @@ function App() {
               </div>
             </div>
           </section>
+          {screen === 'task' && (
+            <div className="modal-overlay">
+            <TaskPage 
+            goBack={() => setScreen('calendar')}
+            day = {selectedDay}
+            month = {month}
+            year = {year}
+            monthList = {monthLittle}
+            />
+            </div>
+          )}
         </div>
-        <section className="main__task"></section>
       </main>
+    </div>
+  );
+}
+
+function TaskPage(props) {
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const date = new Date(props.year, props.month, props.day);
+  const dayIndex = date.getDay();
+  return (
+    <div>
+      <h1>{dayNames[dayIndex]}, {props.monthList[props.month]} {props.day}</h1>
+      <button onClick={props.goBack}>Назад</button>
     </div>
   );
 }
